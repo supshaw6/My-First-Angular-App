2f4576ec95d38a8abde9776bf35bf0bb6a6be4c9
Sophia Upshaw <sophiaupshaw@Sophias-Air.attlocal.net>
2020-11-02 14:46:56 -0500

    my first angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+// Imports
 import { Component } from '@angular/core';
 
+// Component decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+// Component logic
 export class AppComponent {
   title = 'my-first-angular-app';
 }
